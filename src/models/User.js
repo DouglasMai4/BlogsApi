ec2b94b594432c79e1f565db30140e779080be39
Douglas Maia <douglasmai4@protonmail.com>
2024-01-30 12:55:19 -0300

    feat: Created User model

---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    tableName: 'users',
+    timestamps: false,
+    underscored: true,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId', as: 'blogPosts' });
+  };
+
+  return User;
+};
+
+module.exports = UserModel;
